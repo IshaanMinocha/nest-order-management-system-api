712cb12c148b85b079a4fc392fb9f000971c74c2
Ishaan Minocha <minochaishaan2003@gmail.com>
2025-08-22 20:26:46 +0530

    chore: added base tooling, linting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}/api/v1`);
 }
 bootstrap();
